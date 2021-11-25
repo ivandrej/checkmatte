9d809c5b352384c3353a7eb7e80b06374428e9ed
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2021-11-25 17:10:53 +0100

    Fix experiment evaluation script

--- composite.py
+++ composite.py
@@ -8,6 +8,10 @@ python generate_videomatte_with_background_video.py \
 
 import argparse
 import os
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
 import pims
 import numpy as np
 import random
@@ -16,30 +20,32 @@ from tqdm import tqdm
 
 bgrs_per_fgr = 3
 
+
 # Foreground is an img sequence
 # NOT WORKING RN
 def fgr_img_seq():
     # This dir needs to contain fgr and pha
-    fgr_dir = "/home/andivanov/dev/data/RVM_videomatte_evaluation_1920x1080/videomatte_motion/0015" 
+    fgr_dir = "/home/andivanov/dev/data/RVM_videomatte_evaluation_1920x1080/videomatte_motion/0015"
     framenames = sorted(os.listdir(os.path.join(fgr_dir, "fgr")))
-    
+
     num_frames = min(args.num_frames, len(framenames))
     for t in tqdm(range(num_frames)):
         with Image.open(os.path.join(fgr_dir, 'fgr', framenames[base_t + t])) as fgr, \
                 Image.open(os.path.join(fgr_dir, 'pha', framenames[base_t + t])) as pha:
             fgr = fgr.convert('RGB')
             pha = pha.convert('L')
-            
+
             if args.resize is not None:
                 fgr = fgr.resize(args.resize, Image.BILINEAR)
                 pha = pha.resize(args.resize, Image.BILINEAR)
-                
+
         bgr = Image.fromarray(bgrs[t])
         bgr = bgr.resize(fgr.size, Image.BILINEAR)
-        
+
         pha = np.asarray(pha).astype(float)[:, :, None] / 255
         com = Image.fromarray(np.uint8(np.asarray(fgr) * pha + np.asarray(bgr) * (1 - pha)))
         com.save(os.path.join(out_dir, 'com', str(t).zfill(4) + '.png'))
+
 
 """
 Inputs:
@@ -48,24 +54,26 @@ Inputs:
  - pha: m frames
  - num_frames: how many frames to consider
 """
+
+
 def fgr_vid_bgr_vid(bgr_frames, fgr_path, pha_path, out_dir):
     fgr_frames = pims.PyAVVideoReader(fgr_path)
     pha_frames = pims.PyAVVideoReader(pha_path)
-    assert(len(fgr_frames) == len(pha_frames))
+    assert (len(fgr_frames) == len(pha_frames))
 
     num_frames = min(args.num_frames, min(len(fgr_frames), len(bgr_frames)))
     for t in tqdm(range(num_frames)):
         fgr = Image.fromarray(fgr_frames[t])
         pha = Image.fromarray(pha_frames[t])
         pha = pha.convert('L')
-        
+
         if args.resize is not None:
             fgr = fgr.resize(args.resize, Image.BILINEAR)
             pha = pha.resize(args.resize, Image.BILINEAR)
-                
+
         bgr = Image.fromarray(bgr_frames[t])
         bgr = bgr.resize(fgr.size, Image.BILINEAR)
-        
+
         pha = np.asarray(pha).astype(float)[:, :, None] / 255
         com = Image.fromarray(np.uint8(np.asarray(fgr) * pha + np.asarray(bgr) * (1 - pha)))
         com.save(os.path.join(out_dir, str(t).zfill(4) + '.png'))
@@ -78,22 +86,24 @@ Inputs:
  - pha: m frames
  - num_frames: how many frames to consider
 """
+
+
 def fgr_vid_bgr_img(bgr, fgr_path, pha_path, out_dir):
     fgr_frames = pims.PyAVVideoReader(fgr_path)
     pha_frames = pims.PyAVVideoReader(pha_path)
-    assert(len(fgr_frames) == len(pha_frames))
+    assert (len(fgr_frames) == len(pha_frames))
 
     num_frames = min(args.num_frames, len(fgr_frames))
     for t in tqdm(range(num_frames)):
         fgr = Image.fromarray(fgr_frames[t])
         pha = Image.fromarray(pha_frames[t])
         pha = pha.convert('L')
-        
+
         if args.resize is not None:
             fgr = fgr.resize(args.resize, Image.BILINEAR)
             pha = pha.resize(args.resize, Image.BILINEAR)
         bgr = bgr.resize(fgr.size, Image.BILINEAR)
-        
+
         pha = np.asarray(pha).astype(float)[:, :, None] / 255
         com = Image.fromarray(np.uint8(np.asarray(fgr) * pha + np.asarray(bgr) * (1 - pha)))
         com.save(os.path.join(out_dir, str(t).zfill(4) + '.png'))
@@ -102,50 +112,69 @@ def fgr_vid_bgr_img(bgr, fgr_path, pha_path, out_dir):
 """
 Composite multiple foregrounds to multiple backgrounds.
 Right now for each foreground we have 3 backgrounds 
-""" 
+"""
 def composite_multiple_bgr():
-    print(len(bgr_paths), len(fgr_paths), len(pha_paths))
-    assert (len(bgr_paths) == bgrs_per_fgr * len(fgr_paths) == bgrs_per_fgr * len(pha_paths)) 
-    for i, (fgr_path, pha_path) in enumerate(zip(fgr_paths, pha_paths)):
-        for bgr_path in bgr_paths[i * bgrs_per_fgr: (i+1) * bgrs_per_fgr]:   
-            out_dir = os.path.join(args.out_dir, "{}_{}".format(
-                clipname_from_path(pha_path), clipname_from_path(bgr_path)))
-            os.makedirs(out_dir)
-            if bgr_path.endswith(".mp4") or bgr_path.endswith(".MTS") : # video background
-                bgr_frames = pims.PyAVVideoReader(bgr_path) 
-                fgr_vid_bgr_vid(bgr_frames, fgr_path, pha_path, out_dir)
-            else: # image (static) background
-                with Image.open(bgr_path) as bgr:
-                    bgr = bgr.convert('RGB')
-                    fgr_vid_bgr_img(bgr, fgr_path, pha_path, out_dir)
+    for com_paths in clips:
+        print(com_paths.bgr_path)
+        out_dir = os.path.join(args.out_dir, com_paths.clipname)
+        os.makedirs(out_dir)
+        if com_paths.bgr_path.endswith(".mp4") or com_paths.bgr_path.endswith(".MTS"):  # video background
+            bgr_frames = pims.PyAVVideoReader(com_paths.bgr_path)
+            fgr_vid_bgr_vid(bgr_frames, com_paths.fgr_path, com_paths.pha_path, out_dir)
+        else:  # image (static) background
+            with Image.open(com_paths.bgr_path) as bgr:
+                bgr = bgr.convert('RGB')
+                fgr_vid_bgr_img(bgr, com_paths.fgr_path, com_paths.pha_path, out_dir)
+
+
+def com_clipname(pha_path, bgr_path):
+    return "{}_{}".format(clipname_from_path(pha_path), clipname_from_path(bgr_path))
 
 
 def clipname_from_path(path):
     from pathlib import Path
     return Path(path).stem
-            
 
-# Triplets of very dynamic, medium dynamic and static 
+
+# class BackgroundType(Enum):
+#     STATIC = 1
+#     SEMI_DYNAMIC = 2
+#     DYNAMIC = 3
+#
+#
+# @dataclass
+# class Background:
+#     bgr_path: str
+#     bgr_type: BackgroundType
+#
+#
+# @dataclass
+# class Foreground:
+#     fgr_path: str
+#     pha_path: str
+#     bgrs: List[Background]
+
+# Triplets of very dynamic, medium dynamic and static
 bgr_paths = [
-        "/home/andivanov/dev/data/dynamic_backgrounds_youtube/car_1.mp4",
-        "/data/DVM/bg/test/0180.mp4",
-        "/home/andivanov/dev/data/BGM_Image_Backgrounds/classroom_interior_318.br420-1.jpg",
+    "/home/andivanov/dev/data/dynamic_backgrounds_youtube/car_1.mp4",
+    "/data/DVM/bg/test/0180.mp4",
+    "/home/andivanov/dev/data/BGM_Image_Backgrounds/classroom_interior_318.br420-1.jpg",
 
-        "/home/andivanov/dev/data/dynamic_backgrounds_youtube/nature_1.1.mp4",
-        "/data/DVM/bg/test/0010.mp4",
-        "/home/andivanov/dev/data/BGM_Image_Backgrounds/bar_interior_348.death&co__mg_9751-copy.jpg",
+    "/home/andivanov/dev/data/dynamic_backgrounds_youtube/nature_1.1.mp4",
+    "/data/DVM/bg/test/0010.mp4",
+    "/home/andivanov/dev/data/BGM_Image_Backgrounds/bar_interior_348.death&co__mg_9751-copy.jpg",
 
-        "/home/andivanov/dev/data/dynamic_backgrounds_youtube/nature_2.1.mp4",
-        "/data/DVM/bg/test/0010.mp4",
-        "/home/andivanov/dev/data/BGM_Image_Backgrounds/canyon_0369.jpg",
+    "/home/andivanov/dev/data/dynamic_backgrounds_youtube/nature_2.1.mp4",
+    "/data/DVM/bg/test/0010.mp4",
+    "/home/andivanov/dev/data/BGM_Image_Backgrounds/canyon_0369.jpg",
 
-        "/data/our_dynamic_foreground_captures/Captures Andrej/00037.MTS",
-        "/data/DVM/bg/test/0015.mp4",
-        "/home/andivanov/dev/data/BGM_Image_Backgrounds/garage_interior_121.9.-2019-e-3rd-interior-departamento-1.jpg",
+    "/data/our_dynamic_foreground_captures/Captures Andrej/00037.MTS",
+    "/data/DVM/bg/test/0015.mp4",
+    "/home/andivanov/dev/data/BGM_Image_Backgrounds/garage_interior_121.9.-2019-e-3rd-interior-departamento-1.jpg",
 
-        "/data/our_dynamic_foreground_captures/Captures Andrej/00044.MTS",
-        "/data/DVM/bg/test/0043.mp4",
-        "/home/andivanov/dev/data/BGM_Image_Backgrounds/empty_city_115.empty-city-streets-atlanta-georgia-jackson-bridge.jpg"
+    "/data/our_dynamic_foreground_captures/Captures Andrej/00044.MTS",
+    "/data/DVM/bg/test/0043.mp4",
+    "/home/andivanov/dev/data/BGM_Image_Backgrounds/empty_city_115.empty-city-streets-atlanta-georgia-jackson-bridge.jpg"
 ]
 fgr_paths = [
     "/home/andivanov/dev/data/VideoMatte240K/test/fgr/0004.mp4",
@@ -159,8 +188,35 @@ pha_paths = [
     "/home/andivanov/dev/data/VideoMatte240K/test/pha/0003.mp4",
     "/home/andivanov/dev/data/VideoMatte240K/test/pha/0002.mp4",
     "/home/andivanov/dev/data/VideoMatte240K/test/pha/0001.mp4",
-    "/home/andivanov/dev/data/VideoMatte240K/test/pha/0000.mp4",    
+    "/home/andivanov/dev/data/VideoMatte240K/test/pha/0000.mp4",
 ]
+
+"""
+    Contains paths to fgr, pha and bgr we want to composite
+"""
+class CompositedClipPaths:
+    def __init__(self, fgr_path, pha_path, bgr_path):
+        self.fgr_path = fgr_path
+        self.pha_path = pha_path
+        self.bgr_path = bgr_path
+        self.clipname = com_clipname(pha_path, bgr_path)
+
+
+# Maps composition clip name to the corresponding foreground. \
+# Used in evaluation script to find pha and fgr ground truth
+fgr_path_of_com = {}
+pha_path_of_com = {}
+
+assert (len(bgr_paths) == bgrs_per_fgr * len(fgr_paths) == bgrs_per_fgr * len(pha_paths))
+
+# Match foregrounds with backgrounds. Produces a flat list of (fgr, pha, bgr)
+clips: List[CompositedClipPaths] = []
+for i, (fgr_path, pha_path) in enumerate(zip(fgr_paths, pha_paths)):
+    for bgr_path in bgr_paths[i * bgrs_per_fgr: (i + 1) * bgrs_per_fgr]:
+        com = CompositedClipPaths(fgr_path, pha_path, bgr_path)
+        fgr_path_of_com[com.clipname] = fgr_path
+        pha_path_of_com[com.clipname] = pha_path
+        clips.append(com)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- evaluate_experiment.py
+++ evaluate_experiment.py
@@ -32,24 +32,21 @@ python evaluate.py \
     
 An excel sheet with evaluation results will be written to "pred/videomatte_1920x1080/videomatte_1920x1080.xlsx"
 """
-import warnings
-
-def fxn():
-    warnings.warn("deprecated", DeprecationWarning)
-
 import argparse
 import os
-from PIL import Image
-import cv2
-import kornia
+import warnings
+from concurrent.futures import ThreadPoolExecutor
+
 import numpy as np
 import pims
-import xlsxwriter
 import torch
-from concurrent.futures import ThreadPoolExecutor
+import xlsxwriter
+from PIL import Image
 from tqdm import tqdm
 
-from composite import fgr_paths, pha_paths, bgrs_per_fgr
+from composite import fgr_path_of_com, pha_path_of_com
+from evaluation_metrics import MetricMAD, MetricMSE, MetricGRAD, MetricDTSSD
+
 
 # Returns args object
 def parse_args():
@@ -61,8 +58,10 @@ def parse_args():
         'pha_mad', 'pha_mse', 'pha_grad', 'pha_dtssd', 'fgr_mse'])
     return parser.parse_args()
 
+
 # TODO: Take this from argument
 res = (512, 288)
+
 
 class Evaluator:
     def __init__(self, args):
@@ -70,36 +69,37 @@ class Evaluator:
         self.init_metrics()
         self.evaluate()
         self.write_excel()
-        
+
     def init_metrics(self):
         self.mad = MetricMAD()
         self.mse = MetricMSE()
         self.grad = MetricGRAD()
         self.dtssd = MetricDTSSD()
-        
+
     def evaluate(self):
         tasks = []
         position = 0
-        
+
         with ThreadPoolExecutor(max_workers=self.args.num_workers) as executor:
             # for dataset in sorted(os.listdir(self.args.pred_dir)):
             dataset = "dataset"
+            print("Fgr paths: ", fgr_path_of_com)
             for clip in sorted(os.listdir(os.path.join(self.args.pred_dir))):
                 future = executor.submit(self.evaluate_worker, dataset, clip, position)
                 tasks.append((dataset, clip, future))
                 position += 1
-                    
+
         self.results = [(dataset, clip, future.result()) for dataset, clip, future in tasks]
-        
+
     def write_excel(self):
         workbook = xlsxwriter.Workbook(os.path.join(self.args.pred_dir, f'{os.path.basename(self.args.pred_dir)}.xlsx'))
         summarysheet = workbook.add_worksheet('summary')
         metricsheets = [workbook.add_worksheet(metric) for metric in self.results[0][2].keys()]
-        
+
         for i, metric in enumerate(self.results[0][2].keys()):
             summarysheet.write(i, 0, metric)
             summarysheet.write(i, 1, f'={metric}!B2')
-        
+
         for row, (dataset, clip, metrics) in enumerate(self.results):
             for metricsheet, metric in zip(metricsheets, metrics.values()):
                 # Write the header
@@ -110,35 +110,34 @@ class Evaluator:
                         metricsheet.write(0, col + 2, col)
                         colname = xlsxwriter.utility.xl_col_to_name(col + 2)
                         metricsheet.write(1, col + 2, f'=AVERAGE({colname}3:{colname}9999)')
-                        
+
                 metricsheet.write(row + 2, 0, dataset)
                 metricsheet.write(row + 2, 1, clip)
                 metricsheet.write_row(row + 2, 2, metric)
-        
+
         workbook.close()
 
     def evaluate_worker(self, dataset, clip, position):
-        print("Clip, position: ", clip, position)
-        input()
-        true_fgr_frames = pims.PyAVVideoReader(fgr_paths[int(position / bgrs_per_fgr)])
-        true_pha_frames = pims.PyAVVideoReader(pha_paths[int(position / bgrs_per_fgr)])
+        print("Clip: ", clip)
+        true_fgr_frames = pims.PyAVVideoReader(fgr_path_of_com[clip])
+        true_pha_frames = pims.PyAVVideoReader(pha_path_of_com[clip])
         pred_fgr_frames = pims.PyAVVideoReader(os.path.join(self.args.pred_dir, clip, "fgr.mp4"))
         pred_pha_frames = pims.PyAVVideoReader(os.path.join(self.args.pred_dir, clip, "pha.mp4"))
 
-        assert(len(true_fgr_frames) == len(true_pha_frames))
-        assert(len(pred_fgr_frames) == len(pred_pha_frames))
-        
-        metrics = {metric_name : [] for metric_name in self.args.metrics}
-        
+        assert (len(true_fgr_frames) == len(true_pha_frames))
+        assert (len(pred_fgr_frames) == len(pred_pha_frames))
+
+        metrics = {metric_name: [] for metric_name in self.args.metrics}
+
         pred_pha_tm1 = None
         true_pha_tm1 = None
-        
+
         num_frames = min(len(true_fgr_frames), len(pred_fgr_frames))
-        for t in tqdm(range(num_frames)):    
-            true_pha = video_frame_to_numpy(true_pha_frames[t], grayscale=True) 
+        for t in tqdm(range(num_frames), desc=f'{dataset} {clip}'):
+            true_pha = video_frame_to_numpy(true_pha_frames[t], grayscale=True)
             pred_pha = video_frame_to_numpy(pred_pha_frames[t], grayscale=True)
 
-            assert (true_pha.shape == pred_pha.shape) 
+            assert (true_pha.shape == pred_pha.shape)
             if 'pha_mad' in self.args.metrics:
                 metrics['pha_mad'].append(self.mad(pred_pha, true_pha))
             if 'pha_mse' in self.args.metrics:
@@ -152,10 +151,10 @@ class Evaluator:
                     metrics['pha_dtssd'].append(0)
                 else:
                     metrics['pha_dtssd'].append(self.dtssd(pred_pha, pred_pha_tm1, true_pha, true_pha_tm1))
-                    
+
             pred_pha_tm1 = pred_pha
             true_pha_tm1 = true_pha
-            
+
             if 'fgr_mse' in self.args.metrics:
                 true_fgr = video_frame_to_numpy(true_fgr_frames[t])
                 pred_fgr = video_frame_to_numpy(pred_fgr_frames[t])
@@ -164,6 +163,7 @@ class Evaluator:
                 metrics['fgr_mse'].append(self.mse(pred_fgr[true_msk], true_fgr[true_msk]))
 
         return metrics
+
 
 def video_frame_to_numpy(video_frame, grayscale=False):
     img_frame = Image.fromarray(video_frame)
@@ -174,73 +174,10 @@ def video_frame_to_numpy(video_frame, grayscale=False):
     return torch.from_numpy(np.asarray(resized_frame)).float().div_(255)
 
 
-# TODO: Move metrics to separate class
-class MetricMAD:
-    def __call__(self, pred, true):
-        return (pred - true).abs_().mean() * 1e3
-
-
-class MetricMSE:
-    def __call__(self, pred, true):
-        return ((pred - true) ** 2).mean() * 1e3
-
-
-class MetricGRAD:
-    def __init__(self, sigma=1.4):
-        self.filter_x, self.filter_y = self.gauss_filter(sigma)
-        self.filter_x = torch.from_numpy(self.filter_x).unsqueeze(0).cuda()
-        self.filter_y = torch.from_numpy(self.filter_y).unsqueeze(0).cuda()
-    
-    def __call__(self, pred, true):
-        true_grad = self.gauss_gradient(true)
-        pred_grad = self.gauss_gradient(pred)
-        return ((true_grad - pred_grad) ** 2).sum() / 1000
-    
-    def gauss_gradient(self, img):
-        img_filtered_x = kornia.filters.filter2D(img[None, None, :, :], self.filter_x, border_type='replicate')[0, 0]
-        img_filtered_y = kornia.filters.filter2D(img[None, None, :, :], self.filter_y, border_type='replicate')[0, 0]
-        return (img_filtered_x**2 + img_filtered_y**2).sqrt()
-    
-    @staticmethod
-    def gauss_filter(sigma, epsilon=1e-2):
-        half_size = np.ceil(sigma * np.sqrt(-2 * np.log(np.sqrt(2 * np.pi) * sigma * epsilon)))
-        size = np.int(2 * half_size + 1)
-
-        # create filter in x axis
-        filter_x = np.zeros((size, size))
-        for i in range(size):
-            for j in range(size):
-                filter_x[i, j] = MetricGRAD.gaussian(i - half_size, sigma) * MetricGRAD.dgaussian(
-                    j - half_size, sigma)
-
-        # normalize filter
-        norm = np.sqrt((filter_x**2).sum())
-        filter_x = filter_x / norm
-        filter_y = np.transpose(filter_x)
-
-        return filter_x, filter_y
-        
-    @staticmethod
-    def gaussian(x, sigma):
-        return np.exp(-x**2 / (2 * sigma**2)) / (sigma * np.sqrt(2 * np.pi))
-    
-    @staticmethod
-    def dgaussian(x, sigma):
-        return -x * MetricGRAD.gaussian(x, sigma) / sigma**2
-
-
-class MetricDTSSD:
-    def __call__(self, pred_t, pred_tm1, true_t, true_tm1):
-        dtSSD = ((pred_t - pred_tm1) - (true_t - true_tm1)) ** 2
-        dtSSD = dtSSD.sum() / true_t.numel()
-        dtSSD = dtSSD.sqrt()
-        return dtSSD * 1e2
-
-
 if __name__ == '__main__':
     args = parse_args()
 
     with warnings.catch_warnings():
-        # warnings.simplefilter("ignore")
-        # fxn()
+        warnings.simplefilter("ignore")
+        warnings.warn("deprecated", DeprecationWarning)
         Evaluator(args)
