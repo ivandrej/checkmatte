31a5bb9f6551bf23cc39cc42949f020e6229a7f1
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2022-02-24 14:33:43 +0100

    Visualize attention for multiple person spatial locations

--- visualize_attention.py
+++ visualize_attention.py
@@ -5,8 +5,8 @@ import numpy as np
 """
   /outdir
     /attention_visualization
-      - /0 (frame idx)
-         4-7.png (person h = 4, w = 7) 
+      - /0-0 (person h = 0, w = 0)
+         0.png (frame = 0) 
          .
          .
          .
@@ -18,23 +18,27 @@ class Visualizer:
         self.frameidx = 0
 
     # First frame of sequence only
-    # Center pixel only
+    # All pixels
     def __call__(self, attention):
         assert(attention.dim() == 5)
         T, H, W, _, _ = attention.shape
 
-        h, w = H // 2, W // 2
-        attention_matrix = attention[0][h][w].detach().cpu()
-        attention_matrix = attention_matrix * 100
-        # attention_matrix = np.round_(attention_matrix * 100, decimals=2)
-        ax = sns.heatmap(attention_matrix, linewidth=0.5, linecolor='green', annot=True, fmt=".1f")
-        # h and w are swapped in pyplot plots compared to numpy arrays
-        ax.add_patch(Rectangle((w, h), 1, 1, fill=False, edgecolor='red', linewidth=1))
-        figure = ax.get_figure()
-        frame_out_dir = os.path.join(self.outdir, "attention_visualization", f"{self.frameidx}")
-        if not os.path.exists(frame_out_dir):
-            os.makedirs(frame_out_dir)
+        # h = H // 2
+        # w = W // 2
+        for h in range(0, H, 3):
+            for w in range(0, W, 3):
+                attention_matrix = attention[0][h][w].detach().cpu()
+                attention_matrix = attention_matrix * 100
+                # attention_matrix = np.round_(attention_matrix * 100, decimals=2)
+                ax = sns.heatmap(attention_matrix, linewidth=0.5, linecolor='green', annot=True, fmt=".1f")
+                # h and w are swapped in pyplot plots compared to numpy arrays
+                ax.add_patch(Rectangle((w, h), 1, 1, fill=False, edgecolor='red', linewidth=1))
+                figure = ax.get_figure()
 
-        figure.savefig(os.path.join(frame_out_dir, f"{h}-{w}.png"))
-        figure.clear()
+                frame_out_dir = os.path.join(self.outdir, "attention_visualization", f"{h}-{w}")
+                if not os.path.exists(frame_out_dir):
+                    os.makedirs(frame_out_dir)
+
+                figure.savefig(os.path.join(frame_out_dir, f"{self.frameidx}.png"))
+                figure.clear()
         self.frameidx += T
