3d010f1aa268590ae39fed3ae55aec5733e004e9
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2022-03-11 17:42:59 +0100

    Create a helper script to run inference for RVM and attention on all clips in a directory

---
+++ single_use_scripts/rvm_vs_attention_real_videos.py
@@ -0,0 +1,74 @@
+"""
+    Expected directory structure
+    real_test_videos/
+        cinema (clipgroup)
+            andrej_cinema_jump/
+                person/
+                bgr/
+            francesca_cinema_longwalk
+                person/
+                bgr/
+            ..
+"""
+import argparse
+import os
+import sys
+
+import torch
+
+sys.path.append('..')
+from model import model, model_attention_f3
+import rvm_inference, inference
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--clipgroup', type=str, required=True)
+args = parser.parse_args()
+
+input_dir = os.path.join('/media/andivanov/DATA/dynamic_backgrounds_captured/real_test_videos/', args.clipgroup)
+
+# Test RVM
+model = model.MattingNetwork("mobilenetv3").eval().cuda()
+model.load_state_dict(torch.load("/media/andivanov/DATA/training/rvm_mobilenetv3.pth"))
+
+for clip in os.listdir(input_dir):
+    outdir = os.path.join("/media/andivanov/DATA/results/", args.clipgroup, clip, 'rvm')
+
+    rvm_inference.convert_video(
+        model,  # The loaded model, can be on any device (cpu or cuda).
+        input_source=os.path.join(input_dir, clip, 'person'),
+        input_resize=(512, 288),  # [Optional] Resize the input (also the output).
+        downsample_ratio=None,  # [Optional] If None, make downsampled max size be 512px.
+        output_type="png_sequence",  # Choose "video" or "png_sequence"
+        # output_composition=f"{out_dir}/com",
+        output_alpha=f"{outdir}/pha",  # [Optional] Output the raw alpha prediction.
+        # output_foreground=f"{out_dir}/fgr",
+        # [Optional] Output the raw foreground prediction.
+        seq_chunk=12,  # Process n frames at once for better parallelism.
+        progress=True  # Print conversion progress.
+    )
+
+model = model_attention_f3.MattingNetwork("mobilenetv3").eval().cuda()
+model.load_state_dict(torch.load("/media/andivanov/DATA/euler_training/roughly_matched/attention_variants/"
+                                 "f3/finetuned/offset10/res128_lr1e4_B4/checkpoint/stage1/epoch-27.pth"))
+
+for clip in os.listdir(input_dir):
+    outdir = os.path.join("/media/andivanov/DATA/results/", args.clipgroup, clip, 'attention_f3_offset10')
+    matcher = inference.FixedOffsetMatcher(0)
+
+    inference.convert_video(
+        model,  # The loaded model, can be on any device (cpu or cuda).
+        input_source=os.path.join(input_dir, clip, 'person'),
+        matcher=matcher,
+        bgr_source=os.path.join(input_dir, clip, 'bgr'),
+        input_resize=(227, 128),  # [Optional] Resize the input (also the output).
+        downsample_ratio=None,  # [Optional] If None, make downsampled max size be 512px.
+        output_type="png_sequence",  # Choose "video" or "png_sequence"
+        # output_composition=f"{out_dir}/com",
+        output_alpha=f"{outdir}/pha",  # [Optional] Output the raw alpha prediction.
+        bgr_src_pairs=f"{outdir}/bgr_src",
+        # output_attention=f"{out_dir}/attention",
+        # output_foreground=f"{out_dir}/fgr",
+        # [Optional] Output the raw foreground prediction.
+        seq_chunk=12,  # Process n frames at once for better parallelism.
+        progress=True  # Print conversion progress.
+    )
