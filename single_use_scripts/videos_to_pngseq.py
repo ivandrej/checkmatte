ec7bbbf95bf5be3b48f837ce7b634361194e0bba
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2022-03-11 15:36:34 +0100

    Create a helper script to convert a directory of test bgr/person pairs to a directory of png seqs

---
+++ single_use_scripts/videos_to_pngseq.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', type=str, required=True)
+parser.add_argument('--outdir', type=str,
+                    default='/media/andivanov/DATA/dynamic_backgrounds_captured/real_test_videos/')
+args = parser.parse_args()
+
+def to_pngseq(clipname, tag):
+    """
+    :param clipname: Name of video
+    :param tag: 'person' or 'bgr'
+    """
+    vidpath = os.path.join(args.dir, clipname, f'{tag}.mp4')
+    outdir = os.path.join(args.outdir, clipname, tag)
+    os.makedirs(outdir, exist_ok=True)
+
+    os.system(f"ffmpeg -i {vidpath} {outdir}/%04d.png")
+
+for clipname in os.listdir(args.dir):
+    to_pngseq(clipname, 'person')
+    to_pngseq(clipname, 'bgr')
