d6fc27c03aba0af08e9eb6e753dea1b1fcb1de04
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2022-03-19 16:23:54 +0100

    Add downsample_ratio argument to rvm inference script, so I can test on high resolution

--- rvm_run_inference.py
+++ rvm_run_inference.py
@@ -9,7 +9,8 @@ from rvm_inference import convert_video
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-source', type=str, required=True)
 parser.add_argument('--out-dir', type=str, required=True)
-parser.add_argument('--resize', type=int, default=(512, 288), nargs=2)
+parser.add_argument('--resize', type=int, default=None, nargs=2)
+parser.add_argument('--downsample_ratio', type=float, default=None)
 args = parser.parse_args()
 
 if not os.path.exists(args.out_dir):
@@ -23,11 +24,11 @@ convert_video(
     model,  # The loaded model, can be on any device (cpu or cuda).
     input_source=args.input_source,
     input_resize=args.resize,  # [Optional] Resize the input (also the output).
-    downsample_ratio=None,  # [Optional] If None, make downsampled max size be 512px.
+    downsample_ratio=args.downsample_ratio,  # [Optional] If None, make downsampled max size be 512px.
     output_type="png_sequence",  # Choose "video" or "png_sequence"
-    # output_composition=f"{out_dir}/com",
+    output_composition=f"{out_dir}/com",
     output_alpha=f"{out_dir}/pha",  # [Optional] Output the raw alpha prediction.
-    # output_foreground=f"{out_dir}/fgr",
+    output_foreground=f"{out_dir}/fgr",
     # [Optional] Output the raw foreground prediction.
     seq_chunk=12,  # Process n frames at once for better parallelism.
     progress=True  # Print conversion progress.
