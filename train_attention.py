b91818a7e04b633f054be6390ee2c0affbe1de61
Andrej Ivanov <andrej.ivanov.-nd@disneyresearch.com>
2022-04-07 11:32:32 +0200

    Add argument for training f4 noaspp model

--- train_attention.py
+++ train_attention.py
@@ -6,7 +6,7 @@ from torch import multiprocessing as mp
 
 from base_attention_trainer import AbstractAttentionTrainer
 from model import model_attention_addition, model_attention_concat, model_attention_f3, model_attention_f3_f2, \
-    model_attention_f4
+    model_attention_f4, model_attention_f4_noaspp
 
 
 class AttentionAdditionTrainer(AbstractAttentionTrainer):
@@ -65,6 +65,19 @@ class AttentionAdditionTrainer(AbstractAttentionTrainer):
                               {'params': self.model.refiner.parameters(), 'lr': self.args.learning_rate_refiner},
                               {'params': self.model.spatial_attention.parameters(),
                                'lr': self.args.learning_rate_backbone},
+                              {'params': self.model.backbone.parameters(), 'lr': self.args.learning_rate_backbone},
+                              {'params': self.model.aspp.parameters(), 'lr': self.args.learning_rate_aspp}]
+        elif self.args.model_type == 'f4_noaspp':
+            self.model = model_attention_f4_noaspp.MattingNetwork(self.args.model_variant,
+                                                           pretrained_backbone=True,
+                                                           pretrained_on_rvm=self.args.pretrained_on_rvm).to(
+                self.rank)
+
+            self.param_lrs = [{'params': self.model.backbone_bgr.parameters(), 'lr': self.args.learning_rate_backbone},
+                              {'params': self.model.decoder.parameters(), 'lr': self.args.learning_rate_decoder},
+                              {'params': self.model.refiner.parameters(), 'lr': self.args.learning_rate_refiner},
+                              {'params': self.model.spatial_attention.parameters(),
+                               'lr': self.args.learning_rate_backbone},
                               {'params': self.model.backbone.parameters(), 'lr': self.args.learning_rate_backbone}]
                               # {'params': self.model.aspp.parameters(), 'lr': self.args.learning_rate_aspp}]
         else:
@@ -88,7 +101,9 @@ class AttentionAdditionTrainer(AbstractAttentionTrainer):
                               {'params': self.model.aspp.parameters(), 'lr': self.args.learning_rate_aspp}]
 
     def custom_args(self, parser):
-        parser.add_argument('--model-type', type=str, choices=['addition', 'concat', 'f4', 'f3', 'f2_f3'], default='addition')
+        parser.add_argument('--model-type', type=str,
+                            choices=['addition', 'concat', 'f4', 'f4_noaspp', 'f3', 'f2_f3'],
+                            default='addition')
 
 
 if __name__ == '__main__':
